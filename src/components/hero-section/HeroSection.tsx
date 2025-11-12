4388f350658503d2dc928dc108d01cbe68452d40
yuriivintoniak <you@example.com>
2025-11-12 21:15:10 +0200

    feat: add hero title and hero button

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -5,6 +5,12 @@ export default function HeroSection() {
   return (
     <div className={styles.hero}>
       <Header />
+      <h1 className={styles.heroTitle}>
+        The freshest from{"\n"}nature perfectly{"\n"}prepared and delivered{"\n"}to you
+      </h1>
+      <button className={styles.heroButton}>
+        About us
+      </button>
     </div>
   );
 }
