efb52f1a766c366dffe6c477684e7cd04fc9d376
yuriivintoniak <you@example.com>
2026-01-10 17:27:47 +0200

    style(ui): add bottom padding to AccordionContent

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -56,7 +56,7 @@ function AccordionContent({
       className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm animation-duration-[300ms]"
       {...props}
     >
-      <div className={cn("pt-0 pb-0", className)}>{children}</div>
+      <div className={cn("pt-0 pb-4", className)}>{children}</div>
     </AccordionPrimitive.Content>
   )
 }
